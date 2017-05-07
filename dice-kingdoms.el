019f779516b4cef91c33f2d1a1c42eda0426373f
accidentalrebel <accidentalrebel@gmail.com>
2017-05-07 11:29:48 +0800

    Initial set up of dice-kingdoms.el

---
+++ dice-kingdoms.el
@@ -0,0 +1,52 @@
+;;; dice-kingdoms.el --- Dice Kingdoms Game          -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  Juan Karlo Licudine
+
+;; Author: Juan Karlo Licudine <accidentalrebel@gmail.com>
+;; Keywords: games
+;; Version: 0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(defvar dice-kingdoms--play-area-width 100)
+(defvar dice-kingdoms--play-area-height 20)
+(defvar dice-kingdoms--buffer-name "*dice-kingdoms*")
+
+(defun dice-kingdoms ()
+  ""
+  (interactive)
+  (devenv-smart-open-elisp-output-window dice-kingdoms--buffer-name)
+  (erase-buffer)
+  (coordinate-initialize-view-area dice-kingdoms--play-area-width dice-kingdoms--play-area-height "-")
+  (dice-kingdoms--initialize-world)
+  )
+
+(defun dice-kingdoms--initialize-world ()
+  ""
+  (dice-kingdoms--create-territory (random dice-kingdoms--play-area-width) (random dice-kingdoms--play-area-height))
+  )
+
+(defun dice-kingdoms--create-territory (col row)
+  ""
+  (coordinate-place-char-at col row "x")
+  )
+
+(provide 'dice-kingdoms)
+;;; dice-kingdoms.el ends here
